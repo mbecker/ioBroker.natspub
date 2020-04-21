0d2a0261ef926f8f1b35ea433bcf3b2c0fbc7e6e
Mats Becker <mats.becker@gmail.com>
2020-04-21 14:33:36 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"natspub adapter settings": {
+		"en": "Adapter settings for natspub",
+		"de": "Adaptereinstellungen für natspub",
+		"ru": "Настройки адаптера для natspub",
+		"pt": "Configurações do adaptador para natspub",
+		"nl": "Adapterinstellingen voor natspub",
+		"fr": "Paramètres d'adaptateur pour natspub",
+		"it": "Impostazioni dell'adattatore per natspub",
+		"es": "Ajustes del adaptador para natspub",
+		"pl": "Ustawienia adaptera dla natspub",
+		"zh-cn": "natspub的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
